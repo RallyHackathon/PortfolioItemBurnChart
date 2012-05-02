6db32d75c58f24241b54d600b856c266a3d28374
bridge <bridge@rallydev.com>
2012-05-01 23:06:36 -0600

    changed series colors and hover ability. for real this time.

--- js/BurnChartBuilder.js
+++ js/BurnChartBuilder.js
@@ -5,7 +5,7 @@
             componentUpdatable:'Rally.util.ComponentUpdatable'
         },
         build:function (requestedQuery, chartTitle, startDate, endDate, buildFinishedCallback) {
-        	this.endTime = endDate;
+            this.endTime = endDate;
             this.chartTitle = chartTitle;
             this.buildFinishedCallback = buildFinishedCallback;
             this.startTime = startDate;
@@ -85,137 +85,150 @@
 
         _buildChartConfigAndCallback: function(queryResultsData) {
             var lumenize = require('./lumenize');
-                var contextWorkspaceConfig = Rally.environment.getContext().context.scope.workspace.WorkspaceConfiguration;
-                var workspaceConfiguration = {
-                    // Need to grab from Rally for this user
-                    DateFormat:contextWorkspaceConfig.DateFormat,
-                    DateTimeFormat:contextWorkspaceConfig.DateTimeFormat,
-                    //TODO: Have context code fetch these values for the workspace config, instead of hardcoding them
-                    IterationEstimateUnitName:'Points',
-                    // !TODO: Should we use this?
-                    ReleaseEstimateUnitName:'Points',
-                    TaskUnitName:'Hours',
-                    TimeTrackerEnabled:true,
-                    TimeZone:'America/Denver',
-                    WorkDays:'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday'
-                    // They work on Sundays
-                };
-                
-                var acceptedStates = [];
-                if( this.scheduleStateOidAccepted ){
-                    acceptedStates.push( this.scheduleStateOidAccepted );
-                }
-                if( this.scheduleStateOidReleased ){
-                    acceptedStates.push( this.scheduleStateOidReleased );
-                }
+            var contextWorkspaceConfig = Rally.environment.getContext().context.scope.workspace.WorkspaceConfiguration;
+            var workspaceConfiguration = {
+                // Need to grab from Rally for this user
+                DateFormat:contextWorkspaceConfig.DateFormat,
+                DateTimeFormat:contextWorkspaceConfig.DateTimeFormat,
+                //TODO: Have context code fetch these values for the workspace config, instead of hardcoding them
+                IterationEstimateUnitName:'Points',
+                // !TODO: Should we use this?
+                ReleaseEstimateUnitName:'Points',
+                TaskUnitName:'Hours',
+                TimeTrackerEnabled:true,
+                TimeZone:'America/Denver',
+                WorkDays:'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday'
+                // They work on Sundays
+            };
 
-                var burnConfig = {
-                    workspaceConfiguration:workspaceConfiguration,
-                    upSeriesType:'Story Count',
-                    // 'Points' or 'Story Count'
-                    series:[
-                        'up',
-                        'scope'
-                    ],
+            var acceptedStates = [];
+            if (this.scheduleStateOidAccepted) {
+                acceptedStates.push(this.scheduleStateOidAccepted);
+            }
+            if (this.scheduleStateOidReleased) {
+                acceptedStates.push(this.scheduleStateOidReleased);
+            }
 
-                    acceptedStates:acceptedStates,
-                    start:this.startTime,
-                    end:this.endTime,
-                    // Calculated either by inspecting results or via configuration. pastEnd is automatically the last date in results
-                    holidays:[
-                        {
-                            month:12,
-                            day:25
-                        },
-                        {
-                            year:2011,
-                            month:11,
-                            day:26
-                        },
-                        {
-                            year:2011,
-                            month:1,
-                            day:5
-                        }
-                    ]
-                };
+            var burnConfig = {
+                workspaceConfiguration:workspaceConfiguration,
+                upSeriesType:'Story Count',
+                // 'Points' or 'Story Count'
+                series:[
+                    'up',
+                    'scope'
+                ],
 
-                lumenize.ChartTime.setTZPath("");
-                var tscResults = burnCalculator(queryResultsData.Results, burnConfig);
-
-                var categories = tscResults.categories;
-                var series = tscResults.series;
-                var chartConfiguration = {
-                    chart:{
-                        defaultSeriesType:'area',
-                        zoomType: 'xy'
+                acceptedStates:acceptedStates,
+                start:this.startTime,
+                end:this.endTime,
+                // Calculated either by inspecting results or via configuration. pastEnd is automatically the last date in results
+                holidays:[
+                    {
+                        month:12,
+                        day:25
                     },
-                    credits:{
-                        enabled:false
+                    {
+                        year:2011,
+                        month:11,
+                        day:26
                     },
+                    {
+                        year:2011,
+                        month:1,
+                        day:5
+                    }
+                ]
+            };
+
+            lumenize.ChartTime.setTZPath("");
+            var tscResults = burnCalculator(queryResultsData.Results, burnConfig);
+
+            var categories = tscResults.categories;
+            var series = tscResults.series;
+            var chartConfiguration = {
+                chart:{
+                    defaultSeriesType:'area',
+                    zoomType: 'xy'
+                },
+                credits:{
+                    enabled:false
+                },
+                title:{
+                    text:this.chartTitle
+                },
+                subtitle:{
+                    text:''
+                },
+                xAxis:{
+                    categories:categories,
+                    tickmarkPlacement:'on',
+                    tickInterval:Math.floor(categories.length / 13) + 1,
+                    // set as a function of the length of categories
                     title:{
-                        text:this.chartTitle
-                    },
-                    subtitle:{
-                        text:''
-                    },
-                    xAxis:{
-                        categories:categories,
-                        tickmarkPlacement:'on',
-                        tickInterval:Math.floor(categories.length / 13) + 1,
-                        // set as a function of the length of categories
+                        enabled:false
+                    }
+                },
+                yAxis:[
+                    {
                         title:{
-                            enabled:false
-                        }
-                    },
-                    yAxis:[
-                        {
-                            title:{
-                                text:'Hours'
-                            },
-                            labels:{
-                                formatter:function () {
-                                    return this.value / 1;
-                                }
-                            },
-                            min:0
+                            text:'Hours'
                         },
-                        {
-                            title:{
-                                text:burnConfig.upSeriesType
-                            },
-                            opposite:true,
-                            labels:{
-                                formatter:function () {
-                                    return this.value / 1;
-                                }
-                            },
-                            min:0
-                        }
-                    ],
-                    tooltip:{
-                        formatter:function () {
-                            return '' + this.x + '<br />' + this.series.name + ': ' + this.y;
-                        }
-                    },
-                    plotOptions:{
-                        column:{
-                            stacking:null,
-                            lineColor:'#666666',
-                            lineWidth:1,
-                            marker:{
-                                lineWidth:1,
-                                lineColor:'#666666'
+                        labels:{
+                            formatter:function () {
+                                return this.value / 1;
                             }
                         },
-                        area: {
-                            color: "#090"
+                        min:0
+                    },
+                    {
+                        title:{
+                            text:burnConfig.upSeriesType
+                        },
+                        opposite:true,
+                        labels:{
+                            formatter:function () {
+                                return this.value / 1;
+                            }
+                        },
+                        min:0
+                    }
+                ],
+                tooltip:{
+                    formatter:function () {
+                        return '' + this.x + '<br />' + this.series.name + ': ' + this.y;
+                    }
+                },
+                plotOptions:{
+                    series: {
+                        marker: {
+                            enabled: false,
+                            states: {
+                                hover: {
+                                    enabled: true
+                                }
+                            }
                         }
                     },
-                    series:series
-                };
+                    line: {
+                        color: "#000"
+                    },
+                    column:{
+                        stacking:null,
+                        lineColor:'#666666',
+                        lineWidth:1,
+                        marker:{
+                            lineWidth:1,
+                            lineColor:'#666666'
+                        }
+                    },
+                    area: {
+                        color: "#0b0"
+                    }
+                },
+                series:series
+            };
 
-                this.buildFinishedCallback(true, chartConfiguration);
+            this.buildFinishedCallback(true, chartConfiguration);
         }
     });
 })();
